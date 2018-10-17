bb1d3d8b8764061d5f10b16233a39e556a4dd569
Aditya Inapurapu <adityaii@gmail.com>
2018-10-16 19:06:30 -0700

    changed jenkins_home path to a path relative to the project folder

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 sudo docker run --name elasticsearch --hostname elasticsearch -p 9200:9200 -p 9300:9300 -d elasticsearch
-sudo docker run -p 8080:8080 -p 50000:50000 -v $HOME/jenkins_home:/var/jenkins_home --link elasticsearch:es --name jenkins -d adityai/jenkinselastic-docker
+sudo docker run -p 8080:8080 -p 50000:50000 -v `pwd`/jenkins_home:/var/jenkins_home --link elasticsearch:es --name jenkins-elk -d adityai/jenkinselastic-docker
 sudo docker run -e ELASTICSEARCH_URL=http://elasticsearch:9200 -p 5601:5601 --link elasticsearch:es --name kibana -d kibana
-
